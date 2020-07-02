ac4fc781220f17a68584845f5a76d7b7575283a9
612Noah612 <noah.halvorsrod@gmail.com>
2020-07-02 15:18:35 +0200

    Tattoos can be shown and hidden when pressed on the dropdown button

--- app/src/main/java/com/example/inku/Tattoo.java
+++ app/src/main/java/com/example/inku/Tattoo.java
@@ -17,29 +17,67 @@ import java.util.List;
 
 public class Tattoo extends AppCompatActivity {
     boolean opened=false;
-
+    ImageButton tattoo1;
+    ImageButton tattoo2;
+    ImageButton tattoo3;
+    ImageButton tattoo4;
+    ImageButton tattoo5;
+    ImageButton tattoo6;
+    
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_tattoo);
 
+
+
         ImageButton dropdwn = (ImageButton)findViewById(R.id.imageButton4);
         ImageView tattoo = (ImageView)findViewById(R.id.bild);
-
+         tattoo1 = (ImageButton)findViewById(R.id.tattoo1);
+         tattoo2 = (ImageButton)findViewById(R.id.tattoo2);
+         tattoo3 = (ImageButton)findViewById(R.id.tattoo3);
+         tattoo4 = (ImageButton)findViewById(R.id.tattoo4);
+         tattoo5 = (ImageButton)findViewById(R.id.tattoo5);
+         tattoo6 = (ImageButton)findViewById(R.id.tattoo6);
         //get the picture
         Intent intent = getIntent();
         Bitmap bitmap = (Bitmap) intent.getParcelableExtra("key");
         tattoo.setImageBitmap(bitmap);
 
-        //add the tattoos
+
 
         //Open the ViewList
         dropdwn.setOnClickListener(new View.OnClickListener() {
 
             @Override
             public void onClick(View view) {
-               
+                if(opened==false){
+                    showTattoos();
+                    opened = true;
+                }
+                else if(opened==true){
+                   hideTattoos();
+                    opened = false;
+                }
             }
         });
+    }
+    //method to show all the tattoos
+    public void showTattoos(){
+        tattoo1.setVisibility(View.VISIBLE);
+        tattoo2.setVisibility(View.VISIBLE);
+        tattoo3.setVisibility(View.VISIBLE);
+        tattoo4.setVisibility(View.VISIBLE);
+        tattoo5.setVisibility(View.VISIBLE);
+        tattoo6.setVisibility(View.VISIBLE);
+    }
+    //method to hide tattoos
+    public void hideTattoos(){
+        tattoo1.setVisibility(View.INVISIBLE);
+        tattoo2.setVisibility(View.INVISIBLE);
+        tattoo3.setVisibility(View.INVISIBLE);
+        tattoo4.setVisibility(View.INVISIBLE);
+        tattoo5.setVisibility(View.INVISIBLE);
+        tattoo6.setVisibility(View.INVISIBLE);
     }
 }
