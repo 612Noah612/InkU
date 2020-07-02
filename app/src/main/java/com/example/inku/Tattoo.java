1acaf638db7227a752df12ccfcaf034eeba10158
612Noah612 <noah.halvorsrod@gmail.com>
2020-07-02 14:03:11 +0200

    ViewList can be opened and closed with a ImageButton

--- app/src/main/java/com/example/inku/Tattoo.java
+++ app/src/main/java/com/example/inku/Tattoo.java
@@ -5,8 +5,10 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.graphics.Bitmap;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.ImageButton;
 import android.widget.ImageView;
+import android.widget.ListView;
 import android.widget.Spinner;
 import android.widget.TextView;
 
@@ -14,13 +16,17 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class Tattoo extends AppCompatActivity {
+    boolean opened=false;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_tattoo);
 
+        ImageButton dropdwn = (ImageButton)findViewById(R.id.imageButton4);
         ImageView tattoo = (ImageView)findViewById(R.id.imageView3);
+        final ListView listview = (ListView)findViewById(R.id.listview);
+
         //get the picture
         Intent intent = getIntent();
         Bitmap bitmap = (Bitmap) intent.getParcelableExtra("key");
@@ -29,5 +35,19 @@ public class Tattoo extends AppCompatActivity {
         //add the tattoos
 
 
+        dropdwn.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View view) {
+                if(opened==false) {
+                    listview.setVisibility(View.VISIBLE);
+                    opened=true;
+                }
+                else if(opened==true){
+                    listview.setVisibility(View.INVISIBLE);
+                    opened=false;
+                }
+            }
+        });
     }
 }
