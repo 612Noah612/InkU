baa5034b89b505f453ce575d4c562a197c6b8cfd
612Noah612 <noah.halvorsrod@gmail.com>
2020-07-02 13:35:01 +0200

    Spinner removed, ViewList added but not changed anything about it yet

--- app/src/main/java/com/example/inku/Tattoo.java
+++ app/src/main/java/com/example/inku/Tattoo.java
@@ -7,7 +7,11 @@ import android.graphics.Bitmap;
 import android.os.Bundle;
 import android.widget.ImageButton;
 import android.widget.ImageView;
+import android.widget.Spinner;
 import android.widget.TextView;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class Tattoo extends AppCompatActivity {
 
@@ -17,11 +21,13 @@ public class Tattoo extends AppCompatActivity {
         setContentView(R.layout.activity_tattoo);
 
         ImageView tattoo = (ImageView)findViewById(R.id.imageView3);
-
-
-
+        //get the picture
         Intent intent = getIntent();
         Bitmap bitmap = (Bitmap) intent.getParcelableExtra("key");
         tattoo.setImageBitmap(bitmap);
+
+        //add the tattoos
+
+
     }
 }
